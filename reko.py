0625aa3d21269c06ffd8c68de75eba82d77e0a11
mkhaas <meenakshi.kaushik@gmail.com>
2017-05-11 20:27:36 -0700

    Initial Commit

--- reko.py
+++ reko.py
@@ -1,75 +1,68 @@
 #!/usr/bin/env python
 
 import boto3
+import argparse
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
-
-# Upload a new file
-#data = open('/Users/mkaushik/AWS/Cisco/s2.jpg', 'rb')
-#s3.Bucket('mrkrecoko').put_object(Key='s2.jpg', Body=data)
-
-rekognition = boto3.client("rekognition", "us-east-1")
-face = rekognition.detect_faces(
-    Image={
+def detect_faces_and_send_postivity(image, bucket, attributes=['ALL'], region="us-east-1"):
+    rekognition = boto3.client("rekognition", region)
+    face = rekognition.detect_faces(
+        Image={
  #       'Bytes': b'bytes',
-        'S3Object': {
-            'Bucket': 'mrkrecoko',
-            'Name': 's2.jpg',
-#            'Version': 'string'
-        }
-    },
-    Attributes=['ALL'],
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': image,
+#               'Version': 'string'
+            }
+        },
+        Attributes=['ALL'],
+    )
 
-)
+#   print(face, "\n")
+#    print(face["FaceDetails"], "\n")
+#    print(face["FaceDetails"][0], "\n")
+#    print(face["FaceDetails"][0]["Emotions"], "\n")
+#    print(face["FaceDetails"][0]["Smile"], "\n")
+    print ("Smiling: %s" % face["FaceDetails"][0]["Smile"]["Value"])
 
-print(face)
-print("\n")
 
-FEATURES_BLACKLIST = ("Landmarks", "Emotions", "Pose", "Quality", "BoundingBox", "Confidence")
+    inspirationquotes = {
+        "smile" : "Chill the champagne; more good news is on your way :) :)",
+        "not_smile" : "Keep your chin up, universe is sending good news your way :) :)"
+    }
 
-print(face["FaceDetails"])
-print("\n")
+#  print(inspirationquotes, "\n\n")
 
-print(face["FaceDetails"][0])
-print("\n")
+    smilevalue = face["FaceDetails"][0]["Smile"]["Value"]
 
-print(face["FaceDetails"][0]["Emotions"])
-print("\n")
+    if (smilevalue == True):
+#        print (inspirationquotes["smile"])
+        print ('\x1b[1;31m'+ inspirationquotes["smile"] + '\x1b[0m')
+    else:
+        print('\x1b[1;31m'+ inspirationquotes["not_smile"] + '\x1b[0m')
 
-print(face["FaceDetails"][0]["Smile"])
-print("\n")
 
-print(face["FaceDetails"][0]["Smile"]["Value"])
-print("\n")
 
-inspirationquotes = {
-        "smile" : "Chill the champagne; more good news is on your way",
-        "not_smile" : "Keep your chin up, universe is sending good news your way"
-}
+if __name__ == '__main__':
 
-print(inspirationquotes, "\n\n")
+    # Let's use Amazon S3
+    s3 = boto3.resource('s3')
 
-smilevalue = face["FaceDetails"][0]["Smile"]["Value"]
+    # Print out bucket names
+    for bucket in s3.buckets.all():
+        print(bucket.name)
 
-if (smilevalue == True):
-    print (inspirationquote["smile"], "\n")
+    # Find the bucket and image name
+    parser = argparse.ArgumentParser(description='Process image and send positivity')
+    parser.add_argument('-i', '--image', help='Image in S3 to process', required=True)
+    parser.add_argument('-b', '--S3bucket', help='AWS S3 bucket', required=True)
+    args = parser.parse_args()
 
-else:
-    print(inspirationquotes["not_smile"])
+    ## show values ##
+    print ("Image: %s  S3bucket: %s" % (args.image, args.S3bucket))
+#    print ("S3 bucket: %s" % args.S3bucket)
 
-#for emotion in face[""['Emotions']:
-#		print("  {Type} : {Confidence}%".format(**emotion))
+    detect_faces_and_send_postivity(args.image, args.S3bucket)
 
-#for feature, data in face.iteritems():
-#    if feature not in FEATURES_BLACKLIST:
-#        print(feature)
-#        print data[0]
-#        print data[1]
-#        print "  {feature}({data[Value]}) : {data[Confidence]}%".format(feature=feature, data=data)
 
 
